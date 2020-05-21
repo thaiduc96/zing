9ff9bb20e3d71bb4b9f6853185fe2e0f068e43bf
ducthai <dthai@nlstech.net>
2020-05-21 17:57:05 +0700

    first

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Zing.vn</title>
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.1/css/all.css"></link>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+</head>
+<body>
+<header class="header">
+    <div class="page-wrapper">
+        <div class="container-header">
+            <div class="logo">
+                <img src="images/logo-zing-home.svg" alt="">
+                <span>TRI THỨC TRỰC TUYẾN</span>
+                <!-- <p>HỘI XUẤN BẢN VIỆT NAM</p> -->
+            </div>
+            <div class="category-menu">
+                <ul class="header-category-menu">
+                    <li><a href="#" title="2abc"> Xuất bản</a></li>
+                    <li><a href="#" title="2abc"> Sách</a></li>
+                    <li><a href="#" title="2abc"> Tác giả</a></li>
+                    <li><a href="#" title="2abc"> Xã hội</a></li>
+                    <li><a href="#" title="2abc"> Thế giới</a></li>
+                    <li><a href="#" title="2abc"> Kinh doanh</a></li>
+                    <li><a href="#" title="2abc"> Công nghệ</a></li>
+                    <li><a href="#" title="2abc"> Thể thao</a></li>
+                    <li><a href="#" title="2abc"> Giải trí</a></li>
+                    <li><a href="#" title="2abc"> Thời trang</a></li>
+                    <li class="viewMoreMenu more">...</li>
+                </ul>
+            </div>
+            <div class="toolbox">
+                <div class="search-form">
+                    <!--<form action="">-->
+                        <!--<input type="text" class="input-search">-->
+                        <i class="fas fa-search"></i>
+                    <!--</form>-->
+                </div>
+                <div class="avatar">
+                    <img src="https://s120-ava-talk.zadn.vn/0/d/3/c/9/120/86d4ac88d033bfb5d964be6407ae5d0a.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="category-popup jsCategoryPopup">
+        <div class="page-wrapper">
+            <nav class="category-menu">
+                <ul class="list-menu">
+                    <li class="item-menu"><a href="#" title="2abc"> Xã hội</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Thế giới</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Kinh doanh</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Công nghệ</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Thể thao</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Giải trí</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Thời trang</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Đời sống</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item-menu"><a href="#" title="2abc"> Sức khỏe</a>
+                        <div class="subcate">
+                            <ul>
+                                <li>Chính trị</li>
+                                <li>Giáo dục</li>
+                                <li>Giao thông</li>
+                                <li>Pháp luật</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<div class="page-wrapper corona">
+    <div class="corona-weather">
+        <a class="corona" href="#">
+            <strong>CÙNG VƯỢT QUA COVID-19</strong>
+            <span class="corona-country">Việt Nam</span>
+            <div class="corona-status-info">
+                <span>Ca phục hồi: <b> 1.222.222</b></span>
+                <span class="corona-status-info-item">Ca nhiễm: <b>4.222.222</b></span>
+                <span >Ca tử vong: <b>4.222.222</b></span>
+            </div>
+        </a>
+        <div class="weather">
+            <div class="city">
+                <strong>Hà Nội</strong>
+                <span>36°C / 29-38°C</span>
+                <img class="sun" src="https://static-znews.zadn.vn/images/icons/weather/v2/hazy.png">
+            </div>
+        </div>
+    </div>
+    <div class="featured">
+        <div class="content">
+            <div class="newstrending">
+                <?php for($i=0; $i<5; $i++){ ?>
+                <div class="newstrending-item">
+                    <a class="newstrending-image" href="#">
+                        <img src="https://znews-photo.zadn.vn/w210/Uploaded/pwvopivp/2020_05_19/4800.jpg" alt="asdsad">
+                    </a>
+                    <span>Bức ảnh người cha Ấn Độ bật khóc bên đường gây xúc động khắp thế giới</span>
+                </div>
+                <?php } ?>
+            </div>
+            <div class="newsfeatured">
+            <?php for($i=0; $i<3; $i++){ ?>
+                <div class="newsfeatured-item">
+                    <a class="newsfeatured-image" href="#">
+                        <img src="https://znews-photo.zadn.vn/w960/Uploaded/jopltui/2020_05_18/Hien2_1.jpg" alt="asdsad">
+                    </a>
+                    <p>Quân chủng Hải quân đề nghị giảm nhẹ đặc biệt cho ông Nguyễn Văn Hiến</p>
+                    <?php if($i==0) { ?>
+                        <span>Với tư cách bị hại, Quân chủng Hải quân đề nghị xử lý nghiêm nhóm của Đinh Ngọc Hệ và giảm nhẹ đặc biệt cho đô đốc Nguyễn Văn Hiến.</span>
+                        <a href="#" class="related">Ông Nguyễn Văn Hiến bị đề nghị 3-4 năm tù, Út 'Trọc' 20 năm tù</a>
+                    <?php } ?>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+    $(".viewMoreMenu").click(function () {
+        $(".jsCategoryPopup").toggleClass("active");
+    })
+});
